a0b3ca68add5c9c41ec0f35774ec216058d4588c
Vernascoj <Vernascoj@hotmail.com>
2025-06-24 08:35:52 -0700

    Changes

--- estimator-ui.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import EstimatorApp from "./EstimatorApp.js";
-import React from "react";
-
-export function renderUI(container) {
-  const root = createRoot(container);
-  root.render(<EstimatorApp />);
-}
