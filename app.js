6d6eac3d51cef8fa80b24e79dcc4a467d1575e6a
Vernascoj <Vernascoj@hotmail.com>
2025-06-24 08:13:10 -0700

    Remove Files

--- app.js
+++
@@ -1,6 +0,0 @@
-
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
-import EstimatorApp from "./estimator-ui.js";
-
-ReactDOM.render(React.createElement(EstimatorApp), document.getElementById("app"));
