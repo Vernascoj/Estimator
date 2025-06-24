d70069cd241a4c87c3882c37f28814029fd8b98e
Vernascoj <Vernascoj@hotmail.com>
2025-06-24 08:21:08 -0700

    Unload Files

--- estimator-ui.js
+++
@@ -1,8 +0,0 @@
-export function renderUI(container) {
-  container.innerHTML = `
-    <div class="card">
-      <h1>Estimator App</h1>
-      <p>Welcome! The full UI has loaded correctly.</p>
-    </div>
-  `;
-}
