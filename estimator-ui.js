266076d0545491361ed2a07e5a18559a358e2671
Vernascoj <Vernascoj@hotmail.com>
2025-06-24 01:28:47 -0700

    Add files via upload

---
+++ estimator-ui.js
@@ -0,0 +1,10 @@
+
+import React from "https://esm.sh/react";
+export default function EstimatorApp() {
+  return (
+    React.createElement("div", { style: { padding: "2rem", color: "#fff", backgroundColor: "#111", fontFamily: "Arial" } },
+      React.createElement("h1", null, "Estimator App Loaded"),
+      React.createElement("p", null, "This is a working placeholder. Full app functionality goes here.")
+    )
+  );
+}
