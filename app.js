dd2dbcfd0d9c8c36afad7855c84983397d63090b
Vernascoj <Vernascoj@hotmail.com>
2025-06-24 01:34:13 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ import React from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 import EstimatorApp from "./estimator-ui.js";
 
-ReactDOM.render(React.createElement(EstimatorApp), document.getElementById("root"));
+ReactDOM.render(React.createElement(EstimatorApp), document.getElementById("app"));
